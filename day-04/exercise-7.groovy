a0d5a763c8d85aa95b342cc24879fe3bdc3786f6
Alun Davey <hey@4lun.io>
2016-10-24 20:52:49 +0100

    Exercise 7 WIP

---
+++ day-04/exercise-7.groovy
@@ -0,0 +1,13 @@
+double totalBorrowed = Double.parseDouble(
+	System.console().readLine("Enter the total amount borrowed for mortgage: ");
+);
+int yearsToPay = Integer.parseInteger(
+	System.console().readLine("Enter the number of years to pay it back: ");
+);
+double interestRate =  Double.parseDouble(
+	System.console().readLine("Enter the interest rate: ");
+);
+
+double totalAmountToBePaid() {}
+double amountToPayEachYear() {}
+double numOfYearsBeforeInterestPaid() {}
