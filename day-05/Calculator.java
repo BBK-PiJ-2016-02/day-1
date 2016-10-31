d3c69f84aea431768cd047b37295ae38c40efc42
Alun Davey <hey@4lun.io>
2016-10-31 18:44:53 +0000

    Exercise 1

---
+++ day-05/Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator {
+	public double add(int a, int b) {
+		return ((double) a) + ((double) b);
+	}
+
+	public double subtract(int a, int b) {
+		return ((double) a) - ((double) b);
+	}
+
+	public double multiply(int a, int b) {
+		return ((double) a) * ((double) b);
+	}
+
+	public double divide(int a, int b) {
+		return ((double) a) / ((double) b);
+	}
+
+	public double modulus(int a, int b) {
+		return ((double) a) % ((double) b);
+	}
+}
---
+++ day-05/exercise-1.groovy
@@ -0,0 +1,7 @@
+Calculator calc = new Calculator();
+
+println("3 + 4: " + calc.add(3, 4));
+println("3 - 4: " + calc.subtract(3, 4));
+println("3 * 4: " + calc.multiply(3, 4));
+println("3 / 4: " + calc.divide(3, 4));
+println("3 % 4: " + calc.modulus(3, 4));
