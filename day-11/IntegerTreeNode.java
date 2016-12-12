a9240006f42152823cf1897ec7ac70afe68c0534
Alun Davey <hey@4lun.io>
2016-12-12 18:27:04 +0000

    Add IntegerTreeNode class from notes

---
+++ day-11/IntegerTreeNode.java
@@ -0,0 +1,43 @@
+class IntegerTreeNode {
+    int value;
+    IntegerTreeNode left;
+    IntegerTreeNode right;
+
+    public IntegerTreeNode(int value) {
+        this.value = value;
+    }
+
+    public void add(int newNumber) {
+        if (newNumber > this.value) {
+            if (right == null) {
+                right = new IntegerTreeNode(newNumber);
+            } else {
+                right.add(newNumber);
+            }
+        } else {
+            if (left == null) {
+                left = new IntegerTreeNode(newNumber);
+            } else {
+                left.add(newNumber);
+            }
+        }
+    }
+
+    public boolean contains(int n) {
+        if (n == this.value) {
+            return true;
+        } else if (n > this.value) {
+            if (right == null) {
+                return false;
+            } else {
+                return right.contains(n);
+            }
+        } else {
+            if (left == null) {
+                return false;
+            } else {
+                return left.contains(n);
+            }
+        }
+    }
+}
