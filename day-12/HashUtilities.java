f4d29887d1e09b554c82ff5841cece1b91816e49
Alun Davey <hey@4lun.io>
2017-01-16 19:07:59 +0000

    Fix for HashUtilities (issue with negative values revealed by test)

--- day-12/HashUtilities.java
+++ day-12/HashUtilities.java
@@ -1,6 +1,6 @@
 public class HashUtilities {
     public static int shortInt(int value) {
         // Not % 1000 as we can end up with 0 (1000), result must be between 0 & 1000
-        return Math.abs((value % 999) + 1);
+        return (Math.abs(value) % 999) + 1;
     }
 }
