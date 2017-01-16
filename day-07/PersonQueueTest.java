748e546cddec8623a9fe494716b36068248c89cb
Alun Davey <hey@4lun.io>
2017-01-16 20:35:56 +0000

    Implement @Before build up method on PersonQueueTest

--- day-07/PersonQueueTest.java
+++ day-07/PersonQueueTest.java
@@ -2,17 +2,15 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class PersonQueueTest {
-    @Test
-    public void testsInsert() {
-        PersonQueue queue = new SupermarketQueue();
-        Person person = new Person("Bill", 45);
+    PersonQueue queue;
 
-        queue.insert(person);
+    @Before
+    public void buildUp() {
+        queue = new SupermarketQueue();
     }
 
     @Test
     public void testsRetrieve() {
-        PersonQueue queue = new SupermarketQueue();
         Person person = new Person("Jill", 23);
 
         queue.insert(person);
@@ -21,7 +19,6 @@ public class PersonQueueTest {
 
     @Test
     public void testsGetSize() {
-        PersonQueue queue = new SupermarketQueue();
         Person person1 = new Person("Crystal", 26);
         Person person2 = new Person("Roy", 32);
 
