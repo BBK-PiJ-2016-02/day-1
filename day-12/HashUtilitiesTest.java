ec925ab30881230264d9e1c13544c8f484620abd
Alun Davey <hey@4lun.io>
2017-01-16 20:24:24 +0000

    Expanded HashUtilitiesTest to include more test cases

--- day-12/HashUtilitiesTest.java
+++ day-12/HashUtilitiesTest.java
@@ -2,13 +2,54 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class HashUtilitiesTest {
-    @Test
-    public void testsStortInt() {
-        HashUtilities hashUtility = new HashUtilities();
+    HashUtilities hashUtility;
 
-        for(int i = -1337; i < 10000; i++) {
+    @Before
+    public void buildUp() {
+        hashUtility = new HashUtilities();
+    }
+
+    @Test
+    public void tests0() {
+        int shortInt = hashUtility.shortInt(0);
+        assertTrue(isWithinBounds(shortInt));
+    }
+
+    @Test
+    public void tests1() {
+        int shortInt = hashUtility.shortInt(1);
+        assertTrue(isWithinBounds(shortInt));
+    }
+
+    @Test
+    public void tests999() {
+        int shortInt = hashUtility.shortInt(999);
+        assertTrue(isWithinBounds(shortInt));
+    }
+
+    @Test
+    public void tests1000() {
+        int shortInt = hashUtility.shortInt(1000);
+        assertTrue(isWithinBounds(shortInt));
+    }
+
+    @Test
+    public void testsRange() {
+        for(int i = -5000; i < 10000; i++) {
             int shortInt = hashUtility.shortInt(i);
-            assertTrue(shortInt > 0 && shortInt < 1000);
+            assertTrue(isWithinBounds(shortInt));
         }
+    }
+
+    @Test
+    public void testsRandomInts() {
+        for(int i = 0; i < 2000; i++) {
+            int shortInt = hashUtility.shortInt((int) (Math.random() * 1000));
+            assertTrue(isWithinBounds(shortInt));
+        }
+    }
+
+    private boolean isWithinBounds(int value) {
+        return (value > 0 && value < 1000);
     }
 }
