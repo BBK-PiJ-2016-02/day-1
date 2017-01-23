0c32f634cc4e1417c526c58095d60a9d055e11f2
Alun Davey <hey@4lun.io>
2017-01-23 19:20:21 +0000

    Day 15, exercise 2

--- day-15/Exercise2.java
+++ day-15/Exercise2.java
@@ -6,22 +6,34 @@
 
     This is because NullPointerException extends Exception (by extending
     RuntimeException) and Exception is caught in the preceeding catch block.
+
+    # Result
+
+    The error "exception NullPointerException has already been caught" is
+    outputted, with the second catch block highlighted as the source.
+
+    We can see that only the first catch block is executed by the calls to
+    System.out.println, only the first one is ever outputted
 */
+
+import java.util.*;
 
 public class Exercise2 {
     public static void main(String[] args) {
-
+        Exercise2 obj = new Exercise2();
+        obj.launch();
     }
 
     public void launch() {
-        // Some code here
+        List<Integer> list = new ArrayList<Integer>();
         try {
-            // more code here
-            list.add(newElement);
-            // more code here
+            list = null;
+            list.add(1);
         } catch (Exception ex) {
+            System.out.println("# Exception");
             ex.printStackTrace();
         } catch (NullPointerException ex) {
+            System.out.println("# NullPointerException");
             ex.printStackTrace();
         }
     }
