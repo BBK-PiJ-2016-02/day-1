624bebc6ea7419b145ccd8b444b5a4ba65af6526
Alun Davey <hey@4lun.io>
2017-06-07 18:28:00 +0100

    Day 9, exercise 2.2

--- day-09/Exercise2.java
+++ day-09/Exercise2.java
@@ -6,6 +6,16 @@ public class Exercise2 {
 
     public void run() {
         SmartPhone phone = new SmartPhone("Apple");
+
+        /*
+            `MobilePhone phone = new SmartPhone("Apple");`
+
+            2.2 The above won't work as the MobilePhone class does not implement
+            the browseWeb or findPosition methods, a potential solution is to
+            either introduce those methods to that class, remove calls to those
+            methods or switch the type hint to SmartPhone.
+         */
+
         phone.call("01234 567890");
         phone.browseWeb("https://www.google.com/");
         phone.findPosition();
