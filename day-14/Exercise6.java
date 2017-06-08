81a033826badc903f86ceae2f3a163327abd61e6
Alun Davey <hey@4lun.io>
2017-06-08 08:40:12 +0100

    Day 14, exercise 6

--- day-14/Exercise6.java
+++ day-14/Exercise6.java
@@ -4,17 +4,21 @@ public class Exercise6 {
     public static void main(String[] args) {
         Exercise6 obj = new Exercise6();
 
-        List<Integer> unsorted = new ArrayList<Integer>();
+        List<Integer> unsorted = new ArrayList<>();
         unsorted.add(3);
         unsorted.add(7);
-        unsorted.add(2);
+        unsorted.add(3454);
+        unsorted.add(23);
+        unsorted.add(221);
+        unsorted.add(14);
+        unsorted.add(22);
         unsorted.add(9);
         unsorted.add(1);
         List<Integer> sorted = obj.mergeSort(unsorted);
 
         System.out.print("\nUnsorted: ");
-        for(int i = 0; i < sorted.size(); i++) {
-            System.out.print(sorted.get(i) + " ");
+        for(int i = 0; i < unsorted.size(); i++) {
+            System.out.print(unsorted.get(i) + " ");
         }
         System.out.print("\nSorted:   ");
         for(int i = 0; i < sorted.size(); i++) {
@@ -23,18 +27,40 @@ public class Exercise6 {
     }
 
     private List<Integer> mergeSort(List<Integer> list) {
-        // if(list.size() > 1) {
-        //     List<Integer> a = this.mergeSort(list.subList(0, list.size() / 2));
-        //     List<Integer> b = this.mergeSort(list.subList(list.size() / 2, list.size()));
+        int listSize = list.size();
+        if(listSize < 2) return list;
 
-        //     if(a.get(0) > b.get(0)) {
-        //         b.addAll(0, a);
-        //         list = b;
-        //     } else {
-        //         a.addAll(0, b);
-        //         list = a;
-        //     }
-        // }
+        int midPoint = listSize / 2;
+        List<Integer> listA = new ArrayList<>();
+        List<Integer> listB = new ArrayList<>();
+
+        for(int i = 0; i < listSize; i++) {
+            if(i < midPoint) {
+                listA.add(list.get(i));
+            } else {
+                listB.add(list.get(i));
+            }
+        }
+
+        listA = this.mergeSort(listA);
+        listB = this.mergeSort(listB);
+
+        return this.merge(listA, listB);
+    }
+
+    private List<Integer> merge(List<Integer> listA, List<Integer> listB) {
+        List<Integer> list = new ArrayList<>();
+
+        while(listA.size() > 0 && listB.size() > 0) {
+            if(listA.get(0) < listB.get(0)) {
+                list.add(listA.remove(0));
+            } else {
+                list.add(listB.remove(0));
+            }
+        }
+
+        while(listA.size() > 0) list.add(listA.remove(0));
+        while(listB.size() > 0) list.add(listB.remove(0));
 
         return list;
     }
